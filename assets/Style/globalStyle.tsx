21b0c3fd9d06cb46e138e7ebcb254fb34b26904d
Anthony KALBE <anthony.kalbe@cibeo.fr>
2024-07-24 11:24:52 +0200

    adding global style to my app

---
+++ assets/Style/globalStyle.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: 'red',
+    },
+    button: {
+        backgroundColor: 'blue',
+        padding: 10,
+        borderRadius: 5,
+        color : 'white',
+    },
+
+});
